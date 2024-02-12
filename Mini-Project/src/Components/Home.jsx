d8dd9a921a278c83f31b3b9793ee753e55bec23b
Fabinsjr <fabintvm@gmail.com>
2024-02-12 13:29:19 +0530

    minor changes in UI

--- Mini-Project/src/Components/Home.jsx
+++ Mini-Project/src/Components/Home.jsx
@@ -41,25 +41,25 @@ const Home = () => {
 
   return (
     <div>
-      <div className='p-3 d-flex justify-content-around mt-3'>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+      <div className='p-3 d-flex justify-content-around mt-3' >
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 rounded' style={{backgroundColor:"#517EF8"}}>
           <div className='text-center pb-1'>
-            <h4>Admin</h4>
+            <h4 style={{color:"white"}}>Admin</h4>
           </div>
           <hr />
           <div className='d-flex justify-content-between'>
-            <h5>Total:</h5>
-            <h5>{adminTotal}</h5>
+            <h5 style={{color:"white"}}>Total:</h5>
+            <h5 style={{color:"white"}}>{adminTotal}</h5>
           </div>
         </div>
-        <div className='px-3 pt-2 pb-3 border shadow-sm w-25'>
+        <div className='px-3 pt-2 pb-3 border shadow-sm w-25 rounded' style={{backgroundColor:"#517EF8"}}>
           <div className='text-center pb-1'>
-            <h4>Students</h4>
+            <h4 style={{color:"white"}}>Students</h4>
           </div>
           <hr />
           <div className='d-flex justify-content-between'>
-            <h5>Total:</h5>
-            <h5>{employeeTotal}</h5>
+            <h5 style={{color:"white"}}>Total:</h5>
+            <h5 style={{color:"white"}}>{employeeTotal}</h5>
           </div>
         </div>
       </div> 
