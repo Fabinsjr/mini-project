ce77ff29e8f144e00f6399a6f2631f50ffa9a968
Fabinsjr <fabintvm@gmail.com>
2024-02-12 13:35:26 +0530

    changes in student details

--- Mini-Project/src/Components/EmployeeDetail.jsx
+++ Mini-Project/src/Components/EmployeeDetail.jsx
@@ -25,15 +25,15 @@ const EmployeeDetail = () => {
       }
   return (
     <div>
-        <div className="p-2 d-flex justify-content-center shadow">
-            <h4>Student Management System</h4>
+        <div className="p-2 d-flex justify-content-center shadow" style={{backgroundColor: "#5457B0"}}>
+            <h4 style={{color: "white"}}>Student Management System</h4>
         </div>
         <div className='d-flex justify-content-center flex-column align-items-center mt-3'>
-            <img src={`http://localhost:3000/Images/`+employee.image} className='emp_det_image'/>
+            <img src={`http://localhost:3000/Images/`+employee.image} className='emp_det_image' style={{backgroundColor: "grey"}}/>
             <div className='d-flex align-items-center flex-column mt-5'>
                 <h3>Name: {employee.name}</h3>
                 <h3>Email: {employee.email}</h3>
-                <h3>Branch: {employee.category_id}</h3>
+                {/* <h3>Branch: {employee.category_id}</h3> */}
             </div>
             <div>
                 <button className='btn btn-primary me-2'>Edit</button>
